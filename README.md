3fdaad3180e5852e151644c07ab17c085f8d2698
retrolous <menomelas@gmail.com>
2025-07-17 00:12:47 +0100

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # disco-lurker
+
+basic and clunky discord bot that allows you to explore your local music folder (sort of) like you would in a terminal and then queue songs to play. 
+
+as this was only required for an album session with a friend, features such as seeking are absent, but could probably be easily added with ffmpeg arguments.
+
+ffmpeg is required on your system, and accessible in your shell, for this to function.
--- index.js
+++ index.js
@@ -36,12 +36,12 @@ const player = createAudioPlayer();
 
 // when the current song ends
 player.on(AudioPlayerStatus.Idle, () => {
-  try{
-  playlist.shift();
-  playNextSong();
-} catch (error) {
-  console.error(error);
-}
+  try {
+    playlist.shift();
+    playNextSong();
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 // slash commands would probably be better but i'm still stuck in 2020
@@ -62,10 +62,10 @@ client.on("messageCreate", async (message) => {
     resetState();
   }
 
-  // 
+  //
   if (message.content == "-list") {
     currentTextChannelId = message.channel.id;
-    listCurrentDirectory(message)
+    listCurrentDirectory(message);
   }
 
   // output the current folder that the bot is in
@@ -112,149 +112,162 @@ client.on("messageCreate", async (message) => {
     handlePlaying(message);
   }
 
-  if (message.content == ("-playfolder")) {
+  if (message.content == "-playfolder") {
     currentTextChannelId = message.channel.id;
     handleMassPlaying(message);
   }
 });
 
-function handlePlaying(message){
+function handlePlaying(message) {
   if (!connection || connection._state.status === "destroyed") {
-      handleConnection(message);
-    }
+    handleConnection(message);
+  }
 
-    let playArgs = message.content.toString().slice(6);
-    const filePath = pwd + "/" + playArgs;
-    console.log("Path of file:" + filePath);
-    playlist.push(filePath);
+  let playArgs = message.content.toString().slice(6);
+  const filePath = pwd + "/" + playArgs;
+  console.log("Path of file:" + filePath);
+  playlist.push(filePath);
 
-    attemptToPlay();
+  attemptToPlay();
 }
 function handleConnection(message) {
   // ensure that the user is in a channel before attempting to join it
-  if (message.member.voice.channel && message.member.voice.channel.id != currentVoiceChannelId){
+  if (
+    message.member.voice.channel &&
+    message.member.voice.channel.id != currentVoiceChannelId
+  ) {
     // destroy any existing connection and player
     resetState();
     // join the author's channel
     currentVoiceChannelId = message.member.voice.channel.id;
     currentTextChannelId = message.channel.id;
     connection = joinVoiceChannel({
-    channelId: message.member.voice.channel.id,
-    guildId: message.guild.id,
-    adapterCreator: message.guild.voiceAdapterCreator,
-  })
-  connected = true;
-  } else if(message.member.voice.channel.id == currentVoiceChannelId){
-  message.reply("Can only join a different channel");
-  } 
-  else{
+      channelId: message.member.voice.channel.id,
+      guildId: message.guild.id,
+      adapterCreator: message.guild.voiceAdapterCreator,
+    });
+    connected = true;
+  } else if (message.member.voice.channel.id == currentVoiceChannelId) {
+    message.reply("Can only join a different channel");
+  } else {
     message.reply("You need to be in a channel first.");
-  };
+  }
 }
 
-function resetState(){
-  if (connection && connection._state.status !== "destroyed") connection.destroy();
-    player.stop()
-    playlist = [];
-    playing = false;
-    connected = false;
-    currentVoiceChannelId = null;
-    currentTextChannelId = null;
+function resetState() {
+  if (connection && connection._state.status !== "destroyed")
+    connection.destroy();
+  player.stop();
+  playlist = [];
+  playing = false;
+  connected = false;
+  currentVoiceChannelId = null;
+  currentTextChannelId = null;
 }
-
-
 
 function playNextSong() {
   if (playlist.length == 0 && playing) {
     resetState();
   }
 
-  try{
-  const ffmpeg = spawn("ffmpeg", [
-    "-i",
-    playlist[0],
-    "-f",
-    "s16le",
-    "-ar",
-    "48000",
-    "-ac",
-    "2",
-    "pipe:1",
-  ]);
+  try {
+    const ffmpeg = spawn("ffmpeg", [
+      "-i",
+      playlist[0],
+      "-f",
+      "s16le",
+      "-ar",
+      "48000",
+      "-ac",
+      "2",
+      "pipe:1",
+    ]);
 
-  ffmpeg.stderr.on("data", (data) => {
-    console.error(`stderr: ${data}`);
-  });
+    ffmpeg.stderr.on("data", (data) => {
+      console.error(`stderr: ${data}`);
+    });
 
-  const resource = createAudioResource(ffmpeg.stdout, {
-    inputType: StreamType.Raw,
-  });
+    const resource = createAudioResource(ffmpeg.stdout, {
+      inputType: StreamType.Raw,
+    });
 
-  player.play(resource);
-  connection.subscribe(player);
+    player.play(resource);
+    connection.subscribe(player);
 
-  player.on("error", (error) => {
-    console.error(`Error: ${error.message}`);
-  });
-  } catch{
-    client.channels.cache.get(currentTextChannelId).send('There was an error playing your song.');
+    player.on("error", (error) => {
+      console.error(`Error: ${error.message}`);
+    });
+  } catch {
+    client.channels.cache
+      .get(currentTextChannelId)
+      .send("There was an error playing your song.");
     playlist.shift();
-
 
     playNextSong();
   }
 }
 
-function listCurrentDirectory(message){
+function listCurrentDirectory(message) {
   console.log(`'${pwd}'`);
-    let listProcess = spawn("ls", [pwd]);
-    listProcess.stdout.on("data", (data) => {
-      message.reply(data.toString() != "" ? "```" + data.toString() + "```" : "There are no files here.");
-    });
+  let listProcess = spawn("ls", [pwd]);
+  listProcess.stdout.on("data", (data) => {
+    message.reply(
+      data.toString() != ""
+        ? "```" + data.toString() + "```"
+        : "There are no files here."
+    );
+  });
 
-    listProcess.stderr.on("data", (data) => {
-      console.error(`stderr: ${data}`);
-      client.channels.cache.get(currentTextChannelId).send('There was an error listing your directory. Try running -pwd.');
-    });
+  listProcess.stderr.on("data", (data) => {
+    console.error(`stderr: ${data}`);
+    client.channels.cache
+      .get(currentTextChannelId)
+      .send("There was an error listing your directory. Try running -pwd.");
+  });
 }
 
-function changeDirectory(message){
-    console.log(message.content);
-    let cdArgs = message.content.toString().slice(4);
-    pwd += "/" + cdArgs;
+function changeDirectory(message) {
+  console.log(message.content);
+  let cdArgs = message.content.toString().slice(4);
+  pwd += "/" + cdArgs;
 }
 
-function displayQueue(message){
-    let index = 1;
-    let replyMessage = "";
-    playlist.forEach((element) => {
-      replyMessage += `\n${index} - ${element}`;
-      index++;
-    })
-    message.reply(replyMessage == "" ? "There's nothing to play." : "```" + replyMessage + "```");
+function displayQueue(message) {
+  let index = 1;
+  let replyMessage = "";
+  playlist.forEach((element) => {
+    replyMessage += `\n${index} - ${element}`;
+    index++;
+  });
+  message.reply(
+    replyMessage == ""
+      ? "There's nothing to play."
+      : "```" + replyMessage + "```"
+  );
 }
 
-function handleMassPlaying(message){
+function handleMassPlaying(message) {
   if (!connection || connection._state.status === "destroyed") {
-      handleConnection(message);
-    }
+    handleConnection(message);
+  }
 
   let fileList;
   let listProcess = spawn("ls", [pwd]);
-    listProcess.stdout.on("data", (data) => {
-      console.log(data.toString());
-      console.log(data.toString().split("\n"));
-      console.log(data.toString().split("\n").filter(isAudioFile));
-      fileList = data.toString().split("\n").filter(isAudioFile);
+  listProcess.stdout.on("data", (data) => {
+    console.log(data.toString());
+    console.log(data.toString().split("\n"));
+    console.log(data.toString().split("\n").filter(isAudioFile));
+    fileList = data.toString().split("\n").filter(isAudioFile);
 
-      if (fileList.length == 0) message.reply("There are no suitable files here.")
-      fileList.forEach((element) => playlist.push(pwd + `/` + element))
+    if (fileList.length == 0)
+      message.reply("There are no suitable files here.");
+    fileList.forEach((element) => playlist.push(pwd + `/` + element));
 
-      attemptToPlay();
-    });
+    attemptToPlay();
+  });
 }
 
-function attemptToPlay(){
+function attemptToPlay() {
   if (connected && playlist.length > 0 && !playing) {
     playing = true;
     playNextSong();
@@ -262,7 +275,16 @@ function attemptToPlay(){
 }
 function isAudioFile(filePath) {
   // these are just the ones that i have/had in my music collection, add more as you see fit or if ffmpeg supports them
-  const audioExtensions = ['.mp3', '.wav', '.ogg', '.flac', '.aac', '.m4a', '.webm', '.opus'];
-  const extension = filePath.toLowerCase().split('.').pop();
+  const audioExtensions = [
+    ".mp3",
+    ".wav",
+    ".ogg",
+    ".flac",
+    ".aac",
+    ".m4a",
+    ".webm",
+    ".opus",
+  ];
+  const extension = filePath.toLowerCase().split(".").pop();
   return audioExtensions.includes(`.${extension}`);
 }
