42689e8e19035f457a82045f63a993f32e5ff5eb
retrolous <menomelas@gmail.com>
2025-07-16 18:57:25 +0100

    Add joining logic

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
-// Require the necessary discord.js classes
 const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { joinVoiceChannel, createAudioPlayer } = require('@discordjs/voice');
 const { token } = require('./config.json');
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
-// When the client is ready, run this code (only once).
-// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
-// It makes some properties non-nullable.
-client.once(Events.ClientReady, readyClient => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-});
+client.once(Events.ClientReady, readyClient => 
+  {
+	console.log(`${readyClient.user.tag}`);
+  }
+);
 
-// Log in to Discord with your client's token
 client.login(token);
+
+
+let connection;
+const player = createAudioPlayer();
+
+// slash commands would probably be better but i'm still stuck in 2020
+client.on('messageCreate', (message) =>
+  {
+    console.log("message received");
+
+    if (message.author.bot) return false;
+    if (message.content.includes("@here") || message.content.includes("@everyone") || message.type == "REPLY") return false;
+
+    if (message.mentions.has(client.user.id)) {
+        handleConnection(message);
+    }
+  }
+);
+
+function handleConnection(message){
+    if(message.member.voice.channel.id != null && (connection == undefined || connection._state.status == "destroyed")){
+          console.log(`${message.member.voice.channel.id}`)
+
+          connection = joinVoiceChannel({
+          channelId: message.member.voice.channel.id,
+          guildId: message.guild.id,
+          adapterCreator: message.guild.voiceAdapterCreator,
+          });
+        }
+    else {
+      connection.destroy();
+    }
+}
